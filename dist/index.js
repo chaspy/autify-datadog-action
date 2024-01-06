748e0c6b36ca7363335235416b908ccb8c534ea8
kondo takeshi <chaspy@users.noreply.github.com>
2024-01-06 12:28:51 +0900

    compile

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,8 @@ function getResults(inputs) {
         const autify_personal_access_token = inputs.autify_personal_access_token;
         const autify_project_id = inputs.autify_project_id;
         // const datadog_api_key = inputs.datadog_api_key
-        const url = `https://app.autify.com/projects/${autify_project_id}/results`;
+        const per_page = 100;
+        const url = `https://app.autify.com/api/v1/projects/${autify_project_id}/results?per_page=${per_page}`;
         const headers = {
             accept: 'application/json',
             Authorization: `Bearer ${autify_personal_access_token}`

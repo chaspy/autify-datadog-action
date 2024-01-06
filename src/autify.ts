1ad7330aaa739ffeda40a256d3a041ef55014fd5
kondo takeshi <chaspy@users.noreply.github.com>
2024-01-06 12:28:40 +0900

    fix url

--- src/autify.ts
+++ src/autify.ts
@@ -32,8 +32,9 @@ export async function getResults(inputs: Inputs): Promise<getResults | null> {
   const autify_personal_access_token = inputs.autify_personal_access_token
   const autify_project_id = inputs.autify_project_id
   // const datadog_api_key = inputs.datadog_api_key
+  const per_page = 100
 
-  const url = `https://app.autify.com/projects/${autify_project_id}/results`
+  const url = `https://app.autify.com/api/v1/projects/${autify_project_id}/results?per_page=${per_page}`
   const headers = {
     accept: 'application/json',
     Authorization: `Bearer ${autify_personal_access_token}`
